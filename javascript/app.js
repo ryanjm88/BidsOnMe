723a5833a4223f8a233b42f8b88db6a9107a3d08
Ryan McKenzie <ryanjm88@gmail.com>
2018-05-29 19:27:54 -0400

    third push

--- javascript/app.js
+++ javascript/app.js
@@ -5,4 +5,20 @@ $(document).ready(function()    {
     $("#contractorButton").on("click", function()   {
         window.location = "contractor.html";
     });
+    $("#signupButton").on("click", function()   {
+        window.location = "signup.html";
+    });
+
+    $("#homeownerForm").hide();
+    $("#contractorForm").hide();
+
+    $("#homeownerSignUp").on("click", function(event)    {
+        $("#contractorForm").hide();
+        $("#homeownerForm").show();
+    });
+
+    $("#contractorSignUp").on("click", function(event)   {
+        $("#homeownerForm").hide();
+        $("#contractorForm").show();
+    })
 })
