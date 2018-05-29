4eef6e2287ef2244ee88b481ab6fcbbae8abe5f1
Ryan McKenzie <ryanjm88@gmail.com>
2018-05-29 19:05:55 -0400

    second push

---
+++ javascript/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function()    {
+    $("#homeownerButton").on("click", function()    {
+        window.location = "homeowner.html";
+    });
+    $("#contractorButton").on("click", function()   {
+        window.location = "contractor.html";
+    });
+})
