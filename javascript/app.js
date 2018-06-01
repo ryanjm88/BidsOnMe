db5ad5e82b4500595016ea9d86236e85ecb934d4
Ryan McKenzie <ryanjm88@gmail.com>
2018-05-31 20:33:08 -0400

    more updates

--- javascript/app.js
+++ javascript/app.js
@@ -11,6 +11,7 @@ $(document).ready(function()    {
 
     $("#homeownerForm").hide();
     $("#contractorForm").hide();
+    $("#signupDiv2").hide();
 
     $("#homeownerSignUp").on("click", function(event)    {
         $("#contractorForm").hide();
@@ -23,11 +24,46 @@ $(document).ready(function()    {
     });
 
     $("#homeownerPost").hide();
-    $("#homeownerJobs").hide();
+    $("#homeownerJobsCard").hide();
 
+    // getting homeowner sign-up credentials
+
+    $("#submitHomeowner").on("click", function()    {
+        var first_name = $("#first_name").val().trim();
+        var last_name = $("#last_name").val().trim();
+        var email = $("#email").val().trim();
+        var password = $("#password").val().trim();
+
+        console.log(first_name);
+        console.log(last_name);
+        console.log(email);
+        console.log(password);
+
+        $("#homeownerForm").hide();
+        $("#signupDiv").hide();
+        $("#signupDiv2").show();
+    });
+
+    // getting contractor sign-up credentials
+    $("#submitContractor").on("click", function()    {
+        var contractorName = $("#contractorName").val().trim();
+        var contractorEmail = $("#contractorEmail").val().trim();
+        var contractorPassword = $("#contractorPassword").val().trim();
+
+        console.log(contractorName);
+        console.log(contractorEmail);
+        console.log(contractorPassword);
+
+        $("#contractorForm").hide();
+        $("#signupDiv").hide();
+        $("#signupDiv2").show();
+    });
+
+
+    // homeowner member sign in
     $("#homeownerSignIn").on("click", function()    {
         $("#homeownerPost").show();
-        $("#homeownerJobs").show();
+        $("#homeownerJobsCard").show();
         $("#homeownerLogin").hide();
         console.log($("#homeownerEmail").val().trim());
         console.log($("#homeownerPassword").val().trim());
@@ -35,13 +71,25 @@ $(document).ready(function()    {
 
     $('select').formSelect();
 
+    // homeowner new job post info
     $(document).on('click', "#postJob", function() {
-        console.log($("#jobType").val().trim());
-        console.log($("#homeownerAddress").val().trim());
-        console.log($("#homeownerCity").val().trim());
-        console.log($("#homeownerZip").val().trim());
-        console.log($("#startingBid").val().trim());
-        console.log($("#closingDate").val().trim());
-        console.log($("#jobDescription").val().trim());
+        var jobType = $("#jobType").val().trim();
+        var homeownerAddress = $("#homeownerAddress").val().trim();
+        var homeownerCity = $("#homeownerCity").val().trim();
+        var homeownerZip = $("#homeownerZip").val().trim();
+        var startingBid = $("#startingBid").val().trim();
+        var closingDate = $("#closingDate").val().trim();
+        var jobDescription = $("#jobDescription").val().trim();
+
+        console.log(jobType);
+        console.log(homeownerAddress);
+        console.log(homeownerCity);
+        console.log(homeownerZip);
+        console.log(startingBid);
+        console.log(closingDate);
+        console.log(jobDescription);
+
+        // appending new job info to homeowner table
+        $("#jobsPosted").append("<tr><td>" + homeownerAddress + "</td><td>" + jobType + "</td><td>" + jobDescription + "</td></tr>");
     });
 })
