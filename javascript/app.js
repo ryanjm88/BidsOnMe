79198536acaa9b5c1ed7913116122346f6033f9d
Ryan McKenzie <ryanjm88@gmail.com>
2019-02-28 12:06:56 -0500

    tutorial testing

--- javascript/app.js
+++ javascript/app.js
@@ -181,17 +181,17 @@ $(document).ready(function () {
         });
     });
 
-    $("#viewJobs").on("click", function () {
+   // $("#viewJobs").on("click", function () {
 
-        var storedPrice = localStorage.getItem("newerBid");
+     //   var storedPrice = localStorage.getItem("newerBid");
 
-        if (storedPrice === null)
-        console.log(storedPrice);
+       // if (storedPrice === null)
+        //console.log(storedPrice);
 
-        $("#OGprice").html("$" + storedPrice);
+        //$("#OGprice").html("$" + storedPrice);
 
 
-    });
+    //});
 
     $("#jobsPostedContractor").on("click", "#newJobBtn", function (event) {
         event.preventDefault();
